eba6aeeb54935fb9f3d86d45ee61ac1a44c637ee
periadhityan <44988530+periadhityan@users.noreply.github.com>
2024-08-15 16:23:34 +0800

    Minor changes

--- DecryptCaesarCypher.java
+++ DecryptCaesarCypher.java
@@ -13,25 +13,9 @@ public class DecryptCaesarCypher
         System.out.println("The possible original message is: ");
         for(int shift=1;shift<26;shift++)
         {
-            System.out.println(shift+". "+caesarcypher(message, shift));
+            System.out.println(shift+". "+EncryptCaesarCypher.caesarcypher(message, shift));
         }
 
     }
 
-    public static String caesarcypher(String message, int shift)
-    {
-        String new_message = "";
-
-        char[] message_array = message.toCharArray();
-        
-        for(int i=0;i<message_array.length;i++)
-        {
-            char c = message_array[i];
-
-            c = (char)((c - 'a' + shift + 26) % 26 + 'a');
-            new_message += c;
-        }
-
-        return new_message;
-    }
 }
