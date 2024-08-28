28d089c566ac3533a67e5125952f0d8ed5d14a06
periadhityan <44988530+periadhityan@users.noreply.github.com>
2024-08-28 16:40:37 +0800

    Working LFSR for 16 bits input

--- LFSR.java
+++ LFSR.java
@@ -1,12 +1,32 @@
-import java.util.Scanner;
-
 public class LFSR{
 
-    private static Scanner in;
-
-
     public static void main(String[] args){
-        
 
+        int start_state = 0b1001110000101101;
+        int output = start_state;
+        int bit;
+        int period = 0;
+
+        do{
+            bit = ((output>>0)^(output>>2)^(output>>3)^(output>>5)) & 1;
+            output = (output>>1) | (bit<<15);
+            System.out.println("Output  = " + intToBinary(output));
+            period += 1;
+        }while(output != start_state);
+
+        System.out.println("Period = "  + period);
     }
+
+    public static String intToBinary(int n)
+    {
+        String s = "";
+        while (n > 0)
+        {
+            s =  ( (n % 2 ) == 0 ? "0" : "1") +s;
+            n = n / 2;
+        }
+        return s;
+    }
+    
+
 }
