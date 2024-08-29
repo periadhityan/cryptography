f08cf282b3ee3fb116248c9ed4b52e205572fc74
periadhityan <44988530+periadhityan@users.noreply.github.com>
2024-08-29 11:09:42 +0800

    Added random boolean and random bytes output

--- random_numbers.java
+++ random_numbers.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class random_numbers{
 
@@ -18,5 +19,13 @@ public class random_numbers{
         System.out.println("Random integer using Random object = " + randomObj.nextInt(0, 200));
         System.out.println("Random float using Random object = " + randomObj.nextFloat(0, 200));
         System.out.println("Random double using Random object = " + randomObj.nextDouble(0, 200));
+        System.out.println("Random boolean using Random object = " + randomObj.nextBoolean());
+
+        byte[] randomBytes = new byte[20];
+        randomObj.nextBytes(randomBytes);
+
+        System.out.println("Random bytes using Random object = " + Arrays.toString(randomBytes));
+        
+
     }
  }
