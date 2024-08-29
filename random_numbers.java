11baecfc64df8749e729e50e52e58844e02ab767
periadhityan <44988530+periadhityan@users.noreply.github.com>
2024-08-29 10:46:31 +0800

    Added a simple module that generates random number that uses java classes

---
+++ random_numbers.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+ public class random_numbers{
+
+    public static void main(String args[]){
+
+        double myRandom_double = Math.random();
+        int myRandom_integer = (int)(10*Math.random());
+        int myRandom_interval = (int)((30-20)*Math.random() + 20); 
+
+
+        System.out.println("Random double number = " + myRandom_double);
+        System.out.println("Random integer number = " + myRandom_integer);
+        System.out.println("Random integer value between an interval = " + myRandom_interval);
+
+    }
+ }
