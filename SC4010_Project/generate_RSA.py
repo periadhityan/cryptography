b0446fe726ee7056699a4c125a91b1956ae77aa2
periadhityan <44988530+periadhityan@users.noreply.github.com>
2024-11-19 13:46:48 +0800

    Added display of generated RSA keys for vulnerable

--- SC4010_Project/generate_RSA.py
+++ SC4010_Project/generate_RSA.py
@@ -28,19 +28,25 @@ def strong_rsa():
 def vulnerable_rsa():
     e = random_prime(2**16-1, False, 2**15)
     first_loop = True
-    while first_loop or gcd(e, phi) != 1 or N.nbits() != 2048:
+    while first_loop or gcd(e, phi) != 1 or n.nbits() != 2048:
         first_loop = False
         # Note: Product of two 1024-bit primes not always 2048 bit
         # As this is just a proof of concept, we just reject if n < 2048 bits.
         p = random_prime(2**1024-1, False, 2**1023)
         q = random_prime(2**1024-1, False, 2**1023)
-        N = p * q
+        n = p * q
         phi = (p - 1) * (q - 1)
     bezout = xgcd(e, phi)
     d = Integer(mod(bezout[1], phi))
-    n = N.nbits()
     assert mod(d * e, phi) == 1
     # swap the public and private exponent
     e, d = d, e
+
+    print("n = ", n)
+    print()
+    print("e = ", e)
+    print()
+    print("d = ", d)
+    print()
     
-    return N, e, d
+    return n, e, d
